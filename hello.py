054ed4e45b76907b24fa8ccaf2e3d8e342641ed0
Tim Yu <yuyu.tim@gmail.com>
2025-11-02 18:47:31 -0600

    Changed number printing to a loop

--- hello.py
+++ hello.py
@@ -2,9 +2,10 @@
 
 # This prints a hello world function
 def main():
-    num = 10
     print("Hello, World!")
-    print("This is a number:", num)
+
+    for num in range(10, 20):
+        print("This is a number:", num)
 
 if __name__ == "__main__":
     main()
